abb984e9fd8d387976073d87bffc8cdb87f25655
Misael Kosmalski <misaelkosmalski@gmail.com>
2024-05-31 01:18:15 -0300

    Model Order - search bar

--- utils/Btns.js
+++ utils/Btns.js
@@ -5,6 +5,7 @@ class Btns{
         this.btnProduct();
         this.btnClient();
         this.btnOrderService();
+        this.btnOrder();
 
     }
 
@@ -44,7 +45,21 @@ class Btns{
 
     }
 
+    btnOrder(){
+
+        document.querySelector("#btn-order").addEventListener("click", e => {
+
+            this.showPanelOrder();
+            document.querySelector("#title-page").innerHTML = "Pedidos";
+            document.querySelector("#subtitle-page").innerHTML = "Lista de Pedidos";
+
+        });
+
+    }
+
     showPanelProduct(){
+
+        document.querySelector(".background-form").style.display = "block";
 
         document.querySelector("#form-new-product").style.display = "block";
         document.querySelector("#form-update-product").style.display = "none";
@@ -58,9 +73,13 @@ class Btns{
         document.querySelector("#form-update-orderService").style.display = "none";
         document.querySelector("#orderService-list").style.display = "none";
 
+        document.querySelector("#order-list").style.display = "none";
+
     }
 
     showPanelClient(){
+
+        document.querySelector(".background-form").style.display = "block";
 
         document.querySelector("#form-new-product").style.display = "none";
         document.querySelector("#form-update-product").style.display = "none";
@@ -74,9 +93,13 @@ class Btns{
         document.querySelector("#form-update-orderService").style.display = "none";
         document.querySelector("#orderService-list").style.display = "none";
 
+        document.querySelector("#order-list").style.display = "none";
+
     }
 
     showPanelOrderService(){
+
+        document.querySelector(".background-form").style.display = "block";
 
         document.querySelector("#form-new-product").style.display = "none";
         document.querySelector("#form-update-product").style.display = "none";
@@ -89,6 +112,28 @@ class Btns{
         document.querySelector("#form-new-orderService").style.display = "block";
         document.querySelector("#form-update-orderService").style.display = "none";
         document.querySelector("#orderService-list").style.display = "block";
+
+        document.querySelector("#order-list").style.display = "none";
+
+    }
+
+    showPanelOrder(){
+
+        document.querySelector(".background-form").style.display = "none";
+
+        document.querySelector("#form-new-product").style.display = "none";
+        document.querySelector("#form-update-product").style.display = "none";
+        document.querySelector("#product-list").style.display = "none";
+
+        document.querySelector("#form-new-client").style.display = "none";
+        document.querySelector("#form-update-client").style.display = "none";
+        document.querySelector("#client-list").style.display = "none";
+
+        document.querySelector("#form-new-orderService").style.display = "none";
+        document.querySelector("#form-update-orderService").style.display = "none";
+        document.querySelector("#orderService-list").style.display = "none";
+
+        document.querySelector("#order-list").style.display = "block";
 
     }
 
